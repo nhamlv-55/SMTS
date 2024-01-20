b47999ba2b3fe47447651906c1f9932e00e37067
Nham Le <nv3le@uwaterloo.ca>
2024-01-19 23:00:22 -0500

    hardcode the version to avoid git config lock

--- server/version.py
+++ server/version.py
@@ -11,8 +11,8 @@ __author__ = 'Matteo Marescotti'
 if sys.version_info.major != 3 or sys.version_info.minor < 5:
     sys.stderr.write('Python 3.5 is needed!\n')
     sys.exit(-1)
-
-version = int(subprocess.check_output([str(pathlib.Path(__file__).parent.parent / 'version.sh')]).strip())
+version=-1
+# version = int(subprocess.check_output([str(pathlib.Path(__file__).parent.parent / 'version.sh')]).strip())
 
 if __name__ == '__main__':
     print(version)
